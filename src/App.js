32aacc82f001b62f444e52307ed6c8cb39666d71
shasankaborah <shasanka@nordicoverseas.com>
2020-07-20 16:55:53 +0530

    Added Appbar and Drawer

--- src/App.js
+++ src/App.js
@@ -1,42 +1,80 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from "react";
+import { Grid, Drawer, Divider, List , Typography } from "@material-ui/core";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import IconButton from "@material-ui/core/IconButton";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-
-// import React, { Component } from 'react'
-
-import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1> hello</h1>
+        <Grid container>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            // className={clsx(classes.appBar, {
+            //   [classes.appBarShift]: open,
+            // })}
+          >
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                // onClick={handleDrawerOpen}
+                edge="start"
+                // className={clsx(classes.menuButton, {
+                //   [classes.hide]: open,
+                // })}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap>
+                Mini variant drawer
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer variant="permanent">
+            <div>
+              <IconButton>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              {["Normal Mode", "FL Based Mode"].map(
+                (text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                )
+              )}
+            </List>
+            <Divider />
+            <List>
+              {["Decoder Statistics", "Carrier Statistics", "Terminal Statistics" , "IP Output Statistics" , "Settings"].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+        </Grid>
       </div>
-    )
+    );
   }
 }
-
-
-// export default App;
